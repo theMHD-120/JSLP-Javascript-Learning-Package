df78544c53a307ad51000d36c8f9e8cd107a43eb
sm.mahdavi <sm.shuniversity1400gmail.com>
2023-08-27 14:43:48 +0330

    DOM

--- 2. JS Adavnced/S3-/backCode.js
+++ 2. JS Adavnced/S3-/backCode.js
@@ -1,5 +1,13 @@
 // Part 8 ------------------------------------------------------------------------------------------------------------
-// Document Object Model (DOM);
+// Document Object Model (DOM); 
 
+function changeValue() {
+    // document.getElementById("comp1").innerHTML = "somthing on the way";
+    var paragraph = document.getElementsByTagName("p"); 
+    // Now, paragraph is an array of <p>..</p> tags;
 
+    for (var index = 0; index < 3; index++) {
+        paragraph[index].innerHTML = "Paragraph-" + (index + 1);
+    }
+}
 
