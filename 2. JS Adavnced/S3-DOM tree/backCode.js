34afdca1669e96ef0d995fb7ca8c6917412fe4b1
sm.mahdavi <sm.shuniversity1400gmail.com>
2023-09-01 19:07:09 +0330

    DOM-changing

--- 2. JS Adavnced/S3-DOM tree/backCode.js
+++ 2. JS Adavnced/S3-DOM tree/backCode.js
@@ -68,7 +68,7 @@ function childes() {
     attrabuteNodes -->  2 (src, value, title, type and ... (tag attributes));
     textNodes      -->  3 (texts);
     commentNodes   -->  8 (<!--somthing-->);
-    documentNode   -->  9 (the ROOT node (parent of all nodes));
+    documentNode   -->  9 (the ROOT node (parent of all nodes) --> <html>..</html> tag);
     */
 }
 
@@ -112,6 +112,7 @@ function addNewLinks(flagNumber) {
         // Attention: if we want to use <a>..</a> tags that are only in div tag (with id="comp2"), using getElementById("comp2") is necessary;
 
         document.getElementById("comp2").insertBefore(newLink, targets[0]);  // target[0] --> first <a>..</a> tag (link 1);
+        
         /*
         A new method:
         document.getElementById("comp2").replaceChild(newLink, targets[0]);  
@@ -119,6 +120,14 @@ function addNewLinks(flagNumber) {
         You can try this :);
         */
     }
+
+    /*
+    How can we give an event to the new tag (here --> newLink)?
+    With using << event attributes >>;
+    See the code below; 
+    Click on the inserted link (Link 3) and see the result :);
+    */
+    newLink.onclick = function() {alert("Link 3 (new link) clicked!");}
 }
 
 function removeLinks () {
@@ -133,5 +142,12 @@ function removeLinks () {
 
     var parentElemnt = document.getElementById("comp2");
     var children = parentElemnt.getElementsByTagName("a");
-    parentElemnt.removeChild(children[0]);  // to remove the first child;
+    parentElemnt.removeChild(children[0]);  // to remove the first child (first <a>..</a> tag);
+}
+
+function htmlAndBody() {
+    alert(document.documentElement);            // <html>..</html> tag;
+    alert(document.documentElement.innerHTML);  // all in <html>..</html> tag;
+    alert(document.body);                       // <body>..</body> tag;
+    alert(document.body.innerHTML);             // all in <body>..</body> tag;
 }
