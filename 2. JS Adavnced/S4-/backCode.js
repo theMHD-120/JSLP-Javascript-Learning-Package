39c37202331674099d675811fd6fc4c18f4689e1
sm.mahdavi <sm.shuniversity1400gmail.com>
2023-09-03 21:12:46 +0330

    js objects - string - commit 3

--- 2. JS Adavnced/S4-/backCode.js
+++ 2. JS Adavnced/S4-/backCode.js
@@ -5,12 +5,13 @@ function stringObject() {
 
     /*
     Learning about some String methods:
-    1. length, to get length of a string;
-    2. constructor, to get constructor of a string;
-    3. charAt, to get charactor of a string index;
-    4. charCodeAt, to get ascii code of above charactor; 
-    5. concat, to concatinate some strings to a string (infinite);
+        1. length, to get length of a string;
+        2. constructor, to get constructor of a string;
+        3. charAt, to get charactor of a string index;
+        4. charCodeAt, to get ascii code of above charactor; 
+        5. concat, to concatinate some strings to a string (infinite);
     */
+
     var txt1 = "Hi; How are you?";
     alert("The txt1 is:\n" + txt1);
     alert("Length of txt1 is:\n" + txt1.length);                                     // #1;
@@ -19,7 +20,12 @@ function stringObject() {
     alert("The ascii code of fifth charator (index 4) is:\n" + txt1.charCodeAt(4));  // #4;
 
     var txt2 = "\nWhats up?";
-    alert(txt1.concat("\n-Thanks;", " I'm fine;", txt2));
+    alert(txt1.concat("\n-Thanks;", " I'm fine;", txt2));                            // #5;
+    alert("Attention: txt1 did not change ...\n" + "txt1:  " + txt1);
+    /*
+        To change txt1, you should use assignment (see bottom):
+        txt1 = txt1.concat("\n-Thanks;", " I'm fine;", txt2);
+    */
 
 
     function programmer(name, nickName, pLang) {
