ea6f9e94bfcfec77cea4705c268b455012315551
sm.mahdavi <sm.shuniversity1400gmail.com>
2023-09-09 20:42:11 +0330

    js objects - arrays - indexOf and lastIndexOf

--- 2. JS Adavnced/S5-Objects-Array and Math/backCode.js
+++ 2. JS Adavnced/S5-Objects-Array and Math/backCode.js
@@ -13,7 +13,20 @@ function arrayObject() {
     alert("The result of concating carsArray1 with carsArray2 is:\n" + carsArray1.concat(carsArray2));
     alert(carsArray1);
     /*
-        Why carsArray1 not changed?
-            Remember concat method in strings :);
+        Why carsArray1 did not changed?
+            Remember concatenation in strings (concat method) :);
     */  
+
+    alert(carsArray1.indexOf("F"));
+    alert(carsArray1.indexOf("Ferrari"));
+    alert(carsArray1.indexOf("Bugatti", 2));
+    alert(carsArray1.indexOf("bugatti"));
+    /*
+        Ok? this is like the indexOf method on strings;
+        It works for array elements (not a single letter or a substring of an element);
+        ---
+        Also we have the <<< lastIndexOf >>> method for arrays;
+        Like lastIndexOf method in strings;
+        Of course with the above condition (it works for array elements...);
+    */
 }
