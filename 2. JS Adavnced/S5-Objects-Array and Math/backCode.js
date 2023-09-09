40ed44dd33e34896d6dfbd1ea905983f76dd1e00
sm.mahdavi <sm.shuniversity1400gmail.com>
2023-09-09 14:54:01 +0330

    js objects - arrays - lenght and concat

--- 2. JS Adavnced/S5-Objects-Array and Math/backCode.js
+++ 2. JS Adavnced/S5-Objects-Array and Math/backCode.js
@@ -2,5 +2,18 @@
 // JavaScript Objects - Array;
 
 function arrayObject() {
-    
+
+    var array = new Array();
+
+    // Array-object methods -----------------------------------------------------------------
+    var carsArray1 = ["BMW", "Ferrari", "Bugatti", "Lamborghini", "Benz"];
+    alert("The length of carsArray is:\n" + carsArray1.length);
+
+    var carsArray2 = ["Peugeot", "Alfa romeo", "Koenigsegg", "Mitsubishi"]
+    alert("The result of concating carsArray1 with carsArray2 is:\n" + carsArray1.concat(carsArray2));
+    alert(carsArray1);
+    /*
+        Why carsArray1 not changed?
+            Remember concat method in strings :);
+    */  
 }
