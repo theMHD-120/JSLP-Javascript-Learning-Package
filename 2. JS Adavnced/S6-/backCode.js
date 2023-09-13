3e5b5f21f8084783792a9310269046c091ba274d
sm.mahdavi <sm.shuniversity1400gmail.com>
2023-09-13 22:57:03 +0330

    js objects - RegExp object - pattern types

--- 2. JS Adavnced/S6-/backCode.js
+++ 2. JS Adavnced/S6-/backCode.js
@@ -13,21 +13,39 @@ function regExpObject() {
             https://en.wikipedia.org/wiki/Regular_expression
     */
 
+    
     // RegExp object methods -----------------------------------------------------------------------
-    var pattern = /[tshpMeHsD]/
+    var pattern1 = /[tshpMeHsD]/;
+    
     var word1 = "apple";
-    var word2 = "html";
-
-    alert(pattern.test(word1));
+    alert(pattern1.test(word1));
     /*
         If only one of << tshpMeHsD >> pattern letters is
             Existed in the word, test() method returns << true >>;
     */
 
-    alert(pattern.exec(word2));
+    var word2 = "html";
+    alert(pattern1.exec(word2));
     /*
         Returns the first common letter between pattern and the word;
         First letter in the word;
     */
     
+    var word3 = "MHD120";
+    var pattern2 = /[a-z]/;
+    var pattern3 = /[a-z]/i;
+    var pattern4 = /[a-zA-Z]/;
+    var pattern5 = /[^0-9]/;
+    alert(pattern2.test(word3));
+    alert(pattern3.test(word3));
+    alert(pattern4.test(word3));
+    alert(pattern5.test(word3));
+    /*
+        P2 -> Returns << true >> if the word contains lowercase letters;
+        P3 -> Returns << true >> if the word contains letters (case is ignored with << i >> modifier); 
+        P4 -> Returns << true >> if the word contains letters (a to z and A to Z);
+        ---
+        P5 -> Retuens << true >> if there is a non-numeric (one or more) character in the word;
+        Be careful; If there is even one non-numeric character, we are alerted << true >>;
+    */
 }
