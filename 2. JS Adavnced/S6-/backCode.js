ce7f59d802fea8d69df83596355d65ab5b5dacd8
sm.mahdavi <sm.shuniversity1400gmail.com>
2023-09-13 18:36:53 +0330

    js objects - good style

--- 2. JS Adavnced/S6-/backCode.js
+++ 2. JS Adavnced/S6-/backCode.js
@@ -1,2 +1,24 @@
 // Part 14 ------------------------------------------------------------------------------------------------------------
-// JavaScript Objects - Regular expression;
+// JavaScript Objects - Regular expressions (RegExp);
+
+function regExpObject() {
+
+    /*
+        Definition:
+            A regular expression is a sequence of characters that forms a search pattern;
+            Syntax of object defining: /pattern/modifiers;
+        ---
+        See the following links to learn more about regular expressions (RegExp):
+            https://www.w3schools.com/js/js_regexp.asp
+            https://en.wikipedia.org/wiki/Regular_expression
+    */
+
+    // RegExp object methods -----------------------------------------------------------------------
+    var pattern = /[tshpMeHsD]/
+    var word1 = "apple";
+    alert(pattern.test(word1));
+    /*
+        If only one of << tshpMeHsD >> pattern letterns is
+        Existed in the word, test() method returns << true >>;
+    */
+}
