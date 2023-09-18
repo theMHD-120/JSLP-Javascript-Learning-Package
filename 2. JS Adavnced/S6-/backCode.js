c2c2ad43d612d280732c8ca1897fefada3960504
sm.mahdavi <sm.shuniversity1400gmail.com>
2023-09-18 06:05:12 +0330

    readme and js objects - regex - part 14 - better explanation

--- 2. JS Adavnced/S6-/backCode.js
+++ 2. JS Adavnced/S6-/backCode.js
@@ -20,27 +20,27 @@ function regExpObject() {
 
 
     // RegExp (regex) object methods ---------------------------------------------------------------
-    var pattern1 = /[tshpMeHsD]/;
     alert("RegExp (regex) - Methods");
-    
+
+    var pattern1 = /[tshpMeHsD]/;
     var word1 = "apple";
     alert(pattern1.test(word1));
-    /*
-        If even one of << apple >> word letters is
-            Existed in the pattern1, test() method returns << true >>;
-        ---
-        In the next patterns, if there is a common feature between 
-            The word and the pattern, test() method returns << true >>;
-    */
-
     var word2 = "html";
     alert(pattern1.exec(word2));
     /*
-        Returns the first common letter between pattern and the word;
-        First letter in the word;
-        ---
-        In the next patterns, if test() method returns << true >>,
-            exec() method returns the first letter or character that has the common feature;
+        RegExp (regex) methods:
+            1.test():
+                If even one of << apple >> word letters is...
+                    Existed in the pattern1, test() method returns << true >>;
+                ---
+                In the next patterns, if there is a common feature between...
+                    Word (or text) and the pattern, test() method returns << true >>;
+            2.exec():
+                Returns the first common letter between pattern and the word;
+                First letter in the word;
+                ---
+                In the next patterns, if test() method returns << true >>,
+                    exec() method returns the first letter or character that has the common feature;
     */
     
     var word3 = "MHD120";
@@ -48,10 +48,10 @@ function regExpObject() {
     var pattern3 = /[a-z]/i;
     var pattern4 = /[a-zA-Z]/;
     var pattern5 = /[^0-9]/;
-    alert(pattern2.test(word3));
-    alert(pattern3.test(word3));
-    alert(pattern4.test(word3));
-    alert(pattern5.test(word3));
+    alert("Pattern 2: " + pattern2.test(word3));
+    alert("Pattern 3: " + pattern3.test(word3));
+    alert("Pattern 4: " + pattern4.test(word3));
+    alert("Pattern 5: " + pattern5.test(word3));
     /*
         P2 -> Returns << true >> if the word contains lowercase letters;
         P3 -> Returns << true >> if the word contains letters (case is ignored with << i >> modifier); 
@@ -89,6 +89,7 @@ function regExpObject() {
 
     var pattern6 = /a.i/;
     var txt1 = "Ali gets a react api for his project";
+    alert("||| Pattern 6 |||");
     alert(pattern6.test(txt1));
     alert(pattern6.exec(txt1));
     /*
@@ -112,8 +113,10 @@ function regExpObject() {
     var pattern7 = /\w/;
     var pattern8 = /\W/;
     var txt2 = "I'm 20 years old;";
+    alert("||| Pattern 7 |||");
     alert(pattern7.test(txt2));
     alert(pattern7.exec(txt2));
+    alert("||| Pattern 8 |||");
     alert(pattern8.test(txt2));
     alert(pattern8.exec(txt2));
     /*
@@ -136,8 +139,10 @@ function regExpObject() {
 
     var pattern9 = /\d/;
     var pattern10 = /\D/;
+    alert("||| Pattern 9 |||");
     alert(pattern9.test(txt2));
     alert(pattern9.exec(txt2));
+    alert("||| Pattern 10 |||");
     alert(pattern10.test(txt2));
     alert(pattern10.exec(txt2));
     /*
@@ -157,8 +162,10 @@ function regExpObject() {
 
     var pattern11 = /\s/;
     var pattern12 = /\s/;
+    alert("||| Pattern 11 |||");
     alert(pattern11.test(txt2));
     alert(pattern11.exec(txt2));
+    alert("||| Pattern 12 |||");
     alert(pattern12.test(txt2));
     alert(pattern12.exec(txt2));
     /*
@@ -178,8 +185,10 @@ function regExpObject() {
     var pattern13 = /\bjava/;
     var pattern14 = /\Bear/;
     var txt3 = "Here is the javascript learning package;";
+    alert("||| Pattern 13 |||");
     alert(pattern13.test(txt3));
     alert(pattern13.exec(txt3));
+    alert("||| Pattern 14 |||");
     alert(pattern14.test(txt3));
     alert(pattern14.exec(txt3));
     /*
@@ -191,7 +200,7 @@ function regExpObject() {
         9.How does << \B... >> work (explanation with example)? 
             In the second pattern (pattern14), if there is one (or more)...
                 Word(s) containing "ear" in the middle, we are alerted << true >>;
-            What does "In the middle" meaning? 
+            What does "In the middle" mean? 
                 A word in a text (for example txt3) doesn't begin...
                     Or doesn't end with "ear"; But it contains "ear";
     */
--- README.md
+++ README.md
@@ -5,10 +5,10 @@ Author:  seyed mahdi mahdavi mortazavi (theMHD)
 ![JSLP-javascript learning package /// by theMHD](JLPlogo.png)
 
     Learning javascript with javascript codes :)
-    This package contains two parts and serveral sections.
+    This package contains two chpters, six sections and fifteen parts.
 
-    Part 1: Basics of javascript (like another programming languages)
-    Part 2: Special and advanced contents of javascript
+    Chapter 1: Basics of javascript (like another programming languages)
+    Chapter 2: Special and advanced contents of javascript
 
 
     Prerequisites:
