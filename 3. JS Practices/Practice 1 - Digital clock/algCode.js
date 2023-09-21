a20b4697a7acbde6045595402459a94292e87f0f
sm.mahdavi <sm.shuniversity1400gmail.com>
2023-09-22 02:29:52 +0330

    Practice 1 - digi clock - better description and modify  some styles

--- 3. JS Practices/Practice 1 - Digital clock/algCode.js
+++ 3. JS Practices/Practice 1 - Digital clock/algCode.js
@@ -7,10 +7,12 @@
     Designing a digital clock using the JavaScript objects;
     Used objects: 
         - Date 
-        - setInterval (one of inner window objects)
-        - clearInterval (one of inner window objects)
+        - Document
+        - setInterval
+        - clearInterval
 
     In JSLP:
+        Document: JS Advance / Section 3 - DOM tree / Part 8;
         Date: JS Advance / Section 4 - String and Date / Part 11;
         setInterval and clearInterval: 
             JS Advance / Section 6 - Regex and Window / Part 15;
@@ -32,20 +34,31 @@ function digiClock() {
         clearInterval(currTime);
 }
 
-function toggleTimer() {
-    if (document.getElementById("toggle").value == "Pause") {
-        document.getElementById("space").style = "box-shadow: none";
-        document.getElementById("clock").style = "color: rgb(138, 0, 60, 0.6);"
-        document.getElementById("toggle").style = "box-shadow: inset rgba(0, 0, 0, 0.6) 0px 10px 50px;";
-        document.getElementById("toggle").value = "Play";
-        timeFlag = false;
+function toggleStyle(state) {
+    if(!state) {
+        document.getElementById("space").style =  "box-shadow: none;"
+        document.getElementById("clock").style = "text-shadow: none; color: rgb(138, 0, 60, 0.6);"
+        document.getElementById("toggle").style = "box-shadow: inset rgba(0, 0, 0, 0.4) 0px 10px 50px;";
+        document.body.style = "background: linear-gradient(90deg, rgb(156, 1, 161) 0%, rgb(1, 138, 166) 50%, rgb(156, 1, 161) 100%);";
     }
     else {
         document.getElementById("clock").style = "color: rgb(161, 0, 70);";
         document.getElementById("space").style = "box-shadow: rgba(0, 0, 0, 0.6) 0px 30px 50px";
         document.getElementById("toggle").style = "box-shadow: inset rgba(0, 0, 0, 0.6) 0px 1px 5px;";
-        document.getElementById("toggle").value = "Pause";
+        document.body.style = "background: linear-gradient(90deg, rgb(186, 1, 192) 0%, rgba(0,212,255,1) 50%, rgb(186, 1, 192) 100%);";
+    }
+}
+
+function toggleTimer() {
+    if (document.getElementById("toggle").value == "Pause") {
+        toggleStyle(false);
+        timeFlag = false;
+        document.getElementById("toggle").value = "Play";
+    }
+    else {
+        toggleStyle(true);
         timeFlag = true;
+        document.getElementById("toggle").value = "Pause";
     }
 
     digiClock();
